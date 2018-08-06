d8b2d5e52d29ce2a89a601f43bb663d9f387f939
TMcFadden <tmac727@yahoo.com>
2018-08-05 20:15:53 -0400

    update nav bar and links

--- React_Routes/src/index.js
+++ React_Routes/src/index.js
@@ -6,11 +6,54 @@ import Happy from './components/Happy';
 import Sleepy from './components/Sleepy';
 import Guilty from './components/Guilty';
 
-// your code goes here
+// your code goes he
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 class Home extends Component{
   render(){
     return(
       // your code goes here
+      <Router>
+   <div>
+     <ul>
+       <li>
+         <Link to="/">Home</Link>
+       </li>
+       <li>
+         <Link to="/Happy">Happy</Link>
+       </li>
+       <li>
+         <Link to="/Sleepy">Sleepy</Link>
+       </li>
+       <li>
+         <Link to="/Guilty">Guilty</Link>
+       </li>
+     </ul>
+
+     <hr />
+
+     <Route exact path="/" component={Home} />
+     <Route path="/Happy" component={Happy} />
+     <Route path="/Sleepy" component={Sleepy} />
+     <Route path="/Guilty" component={Guilty} />
+   </div>
+ </Router>
+);
+      const Home = () => (
+  <div>
+    <h2>Home</h2>
+  </div>
+);
+
+const About = () => (
+  <div>
+    <h2>About</h2>
+  </div>
+);
+
+const Topics = ({ match }) => (
+  <div>
+    <h2>Topics</h2>
+    </div>
     )
   }
 }
